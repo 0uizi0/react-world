901ec7fc5939b99d7854db9ede07455883428432
0uizi0 <0uizi0@naver.com>
2023-09-14 00:53:03 +0900

    feat: 로그인 API 연동

---
+++ src/app/login/page.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import Link from 'next/link'
+import React, { useState } from 'react'
+import Cookies from 'js-cookie'
+import { loginApi } from '../../utils/api'
+import { useForm } from '../../utils/useForm'
+
+const Login = () => {
+  const { formData, handleChange } = useForm({
+    email: '',
+    password: '',
+  })
+  const [error, setError] = useState<{ [key: string]: string[] } | null>(null)
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    try {
+      const resp = await loginApi(formData)
+      Cookies.set('authToken', resp.user.token)
+
+      window.location.href = '/'
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.errors) {
+        setError(error.response.data.errors)
+      } else {
+        setError({ general: ['서버 오류 발생'] })
+      }
+      console.error(
+        '로그인 실패',
+        error.response ? error.response.data : '서버 오류 발생',
+      )
+    }
+  }
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign in</h1>
+            <p className="text-xs-center">
+              <Link href="/register">Need an account?</Link>
+            </p>
+            {error !== null && (
+              <ul className="error-messages">
+                {Object.keys(error).map((key) =>
+                  error[key].map((errorMessage) => (
+                    <li key={key}>
+                      {key.charAt(0).toUpperCase() + key.slice(1)}{' '}
+                      {errorMessage}
+                    </li>
+                  )),
+                )}
+              </ul>
+            )}
+            <form onSubmit={handleSubmit}>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  onChange={handleChange}
+                  value={formData.email}
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  onChange={handleChange}
+                  value={formData.password}
+                />
+              </fieldset>
+              <button
+                type="submit"
+                className="btn btn-lg btn-primary pull-xs-right"
+              >
+                Sign in
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,11 +6,30 @@ interface RegisterFormData {
   password: string
 }
 
+interface LoginFormData {
+  email: string
+  password: string
+}
+
 export const registerApi = async (formData: RegisterFormData) => {
   try {
     const resp = await axios.post(process.env.NEXT_PUBLIC_API_URL + `users`, {
       user: formData,
     })
+    return resp.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const loginApi = async (formData: LoginFormData) => {
+  try {
+    const resp = await axios.post(
+      process.env.NEXT_PUBLIC_API_URL + `users/login`,
+      {
+        user: formData,
+      },
+    )
     return resp.data
   } catch (error) {
     throw error
